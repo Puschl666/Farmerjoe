ebdfe4c9414b7dca6ae5cfe2abaddcb11b1a0e7b
shadowdiver <graf-dooku@gmx.de>
2015-05-18 22:57:32 +0200

    Added User Script, JSON support and Day/Night Rythm

---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,8 @@
+public class User
+{
+	public int id { get; set; }
+	public string name { get; set; }
+	public int energy { get; set; }
+	public int maxEnergy { get; set; }
+	public string asset { get; set; }
+}
